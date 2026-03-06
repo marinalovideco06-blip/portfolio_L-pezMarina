c783f18df5182207dfa38956ebd3f80b4850fe4b
marinalovideco06-blip <marinalovideco06@gmail.com>
2026-03-06 13:32:32 +0100

    Actualizacion 27

--- src/views/WorkDetail.vue
+++ src/views/WorkDetail.vue
@@ -15,16 +15,17 @@ const work = works.find(w => w.id === id)
     <img 
       src="/imagenes/trabajos/degradado_blanco_vertical.PNG" 
       alt=""
-      class="absolute inset-0 w-full z-0 pointer-events-none"
-    >
+      class="absolute inset-0 w-full h-full z-0 pointer-events-none bg-no-repeat bg-cover bg-center"
+      
+      >
 
-    <div class="relative z-20 text-black flex flex-col items-center min-h-screen">
+    <div class="relative z-20 text-black flex flex-col items-center min-h-screen px-6 sm:px-10">
 
-      <h1 class="text-7xl mt-10">
+      <h1 class="text-3xl sm:text-5xl lg:text-7xl mt-10 text-center">
         {{ work?.title }}
       </h1>
    
-      <div class="mt-20 w-full max-w-5xl px-10">
+      <div class="mt-10 sm:mt-20 w-full max-w-5xl px-4 sm:px-10">
 
         <Carousel
           :opts="{ align: 'start', loop: true }"
@@ -35,7 +36,7 @@ const work = works.find(w => w.id === id)
             <CarouselItem
               v-for="(img, index) in work?.images"
               :key="index"
-              class="min-w-100 mr-6"
+              class="min-w-55 sm:min-w-[320px] lg:min-w-105 mr-6"
             >
               <div class="aspect-square rounded-2xl overflow-hidden shadow-lg bg-white">
                 <img
@@ -49,12 +50,12 @@ const work = works.find(w => w.id === id)
         </Carousel>
       </div>
 
-      <p class="mt-5 px-10 text-center">
+      <p class="mt-10 text-sm sm:text-base lg:text-lg px-4 sm:px-20 text-center max-w-3xl">
         {{ work?.description }}
       </p>
 
      <RouterLink :to="`/${work?.section}`">
-        <button class="z-20 mt-20 border-2 rounded-full border-black px-2 py-2 text-black hover:bg-[rgb(24,61,246)] hover:border-[rgb(24,61,246) hover:text-white hover:border-[rgb(24,61,246)]">EXIT</button>
+        <button class="mt-16 sm:mt-20 border-2 rounded-full border-black px-4 py-2 text-black hover:bg-[rgb(24,61,246)] hover:border-[rgb(24,61,246)] hover:text-white">EXIT</button>
       </RouterLink>
 
     </div>
