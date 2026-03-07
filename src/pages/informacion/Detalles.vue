10464f4b8a2848de9c1b562e6627dfc8884f9053
marinalovideco06-blip <marinalovideco06@gmail.com>
2026-03-07 14:35:38 +0100

    Programación base termianda

--- src/pages/informacion/Detalles.vue
+++ src/pages/informacion/Detalles.vue
@@ -22,22 +22,24 @@ const aplicacion = ref<Apps | undefined>(
   <img 
       src="/imagenes/trabajos/degradado_blanco_vertical.PNG" 
       alt=""
-      class="absolute inset-0 w-full z-0 pointer-events-none"
+      class="absolute inset-0 w-full h-full z-0 pointer-events-none"
     >
-    <div v-if="aplicacion"  class="flex flex-col lg:flex-col lg:justify-between md:items-center w-full max-w-1400px md:mx-auto my-8 gap-8 px-5 md:px-20">
+    <div v-if="aplicacion"  class="flex flex-col items-center w-full max-w-5xl mx-auto my-8 gap-10 px-6 lg:px-20">
       
       
       <div class="flex flex-col items-center">
-	<h1 class="text-8xl relative z-20 mb-10"> 
+	<h1 class="text-4xl sm:text-6xl lg:text-8xl relative z-20 mb-10 text-center"> 
 		{{ aplicacion.name }}
 	</h1>
 
-  <RouterLink to="/info/gallery" class="relative z=20 mt-10">
-    <button class="font-[Urbanist] rounded-full bg-black hover:bg-[rgb(24,61,264)] text-white px-2 py-2">EXIT</button>
+  <RouterLink 
+  to="/info/gallery" 
+  class="relative z-20 mt-10">
+    <button class="font-[Urbanist] rounded-full bg-black hover:bg-[rgb(24,61,246)] text-white px-2 py-2">EXIT</button>
   </RouterLink>
 
 	<div class="relative z-20 font-bold">
-		<p class="text-lg mt-10">
+		<p class="text-base lg:text-lg mt-6 max-w-2xl text-center">
 			{{ aplicacion.description }}
 		</p>
                 
@@ -48,7 +50,7 @@ const aplicacion = ref<Apps | undefined>(
   <img 
     :src="`${aplicacion.icon}`"
     :alt="aplicacion.name"
-    class="w-60 object-contain"
+    class="w-40 sm:w-52 lg:w-60 object-contain"
   />
 </figure>
     </div>
