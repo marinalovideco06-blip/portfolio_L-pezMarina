aa224d459f7d83b76600539a6e52858885ee15a6
marinalovideco06-blip <marinalovideco06@gmail.com>
2026-03-09 13:02:09 +0100

    Cambio de imagenes

--- src/data/works.ts
+++ src/data/works.ts
@@ -11,7 +11,7 @@ export const works = [
       "/imagenes/trabajos/pagina2_alicia.png",
       "/imagenes/trabajos/pagina3_alicia.png",
       "/imagenes/trabajos/pagina4_alicia.png",
-    ], image: "/imagenes/trabajos/portada_alicia.png", description: "Desarrollar un conjunto de piezas para un libro ilustrado a partir de una obra literaria narrativa seleccionada, atendiendo al público lector y a las convenciones editoriales. La propuesta integrará una base pictórica manual (acrílico o gouache) y un acabado digital para maquetación y retoques." },
+    ], image: "/imagenes/trabajos/pagina1_alicia.png", description: "Desarrollar un conjunto de piezas para un libro ilustrado a partir de una obra literaria narrativa seleccionada, atendiendo al público lector y a las convenciones editoriales. La propuesta integrará una base pictórica manual (acrílico o gouache) y un acabado digital para maquetación y retoques." },
   
   { id: 2, section: "ilustracion", title: "36 Days of Type", images: [
       "/imagenes/trabajos/letras_1.png",
@@ -32,14 +32,6 @@ export const works = [
       "/imagenes/trabajos/kaoka_10.png",
       "/imagenes/trabajos/kaoka_11.png",
       "/imagenes/trabajos/kaoka_12.png",
-      "/imagenes/trabajos/kaoka_13.png",
-      "/imagenes/trabajos/kaoka_14.png",
-      "/imagenes/trabajos/kaoka_15.png",
-      "/imagenes/trabajos/kaoka_16.png",
-      "/imagenes/trabajos/kaoka_17.png",
-      "/imagenes/trabajos/kaoka_18.png",
-      "/imagenes/trabajos/kaoka_19.png",
-      "/imagenes/trabajos/kaoka_20.png",
     ], image: "/imagenes/trabajos/kaoka_1.png", description: "Kaoka es una amrca de chocolates familiar y accesible, que busca representar sus mezclas de sabores unicos y poder compartir su sabor con la gente." },
   { id: 4, section: "diseno", title: "Articulo Salud Mental",  images: [
       "/imagenes/trabajos/articulo_1.png",
@@ -49,25 +41,25 @@ export const works = [
     ], image: "/imagenes/trabajos/articulo_1.png", description: "Articulo sobre la salud mental de los diseñadores, maquetado en 2 pliegos a una tinta" },
   
      { id: 5, section: "ilustracion", title: "Aventureros",  images: [
-      "/imagenes/trabajos/ilustracion_1.jpg",
-      "/imagenes/trabajos/ilustracion_2.jpg",
-    ], image: "/imagenes/trabajos/ilustracion_1.jpg", description: "Ilustraciones destinadas a un cuento infantil sobre tres pequeños aventuros de fantasía por un bosque encantado. La pandilla compuesta por seres con formas amables y diseños marcados para ser facilmente reconocibles." },
+      "/imagenes/trabajos/ilustracion_1.png",
+      "/imagenes/trabajos/ilustracion_2.png",
+    ], image: "/imagenes/trabajos/ilustracion_1.png", description: "Ilustraciones destinadas a un cuento infantil sobre tres pequeños aventuros de fantasía por un bosque encantado. La pandilla compuesta por seres con formas amables y diseños marcados para ser facilmente reconocibles." },
   
 
     { id: 6, section: "diseno", title: "Mi juego vitamina",  images: [
-      "/imagenes/trabajos/transversal_1.jpg",
-      "/imagenes/trabajos/transversal_2.jpg",
-      "/imagenes/trabajos/transversal_3.jpg",
-      "/imagenes/trabajos/transversal_4.jpg",
-      "/imagenes/trabajos/transversal_5.jpg",
-      "/imagenes/trabajos/transversal_6.jpg",
-      "/imagenes/trabajos/transversal_7.jpg",
-      "/imagenes/trabajos/transversal_8.jpg",
-      "/imagenes/trabajos/transversal_9.jpg",
-      "/imagenes/trabajos/transversal_10.jpg",
-      "/imagenes/trabajos/transversal_11.jpg",
-      "/imagenes/trabajos/transversal_12.jpg",
-    ], image: "/imagenes/trabajos/transversal_8.jpg", description: "Mi juego vitamina tiene como objetivo ser un juego colaborativo para poder explorar nuevas formas de cuidar nuestra mente y como evitar conductas toxicas mediante un juego accesible para mayores de 12 años." },
+      "/imagenes/trabajos/transversal_1.png",
+      "/imagenes/trabajos/transversal_2.png",
+      "/imagenes/trabajos/transversal_3.png",
+      "/imagenes/trabajos/transversal_4.png",
+      "/imagenes/trabajos/transversal_5.png",
+      "/imagenes/trabajos/transversal_6.png",
+      "/imagenes/trabajos/transversal_7.png",
+      "/imagenes/trabajos/transversal_8.png",
+      "/imagenes/trabajos/transversal_9.png",
+      "/imagenes/trabajos/transversal_10.png",
+      "/imagenes/trabajos/transversal_11.png",
+      "/imagenes/trabajos/transversal_12.png",
+    ], image: "/imagenes/trabajos/transversal_1.png", description: "Mi juego vitamina tiene como objetivo ser un juego colaborativo para poder explorar nuevas formas de cuidar nuestra mente y como evitar conductas toxicas mediante un juego accesible para mayores de 12 años." },
   
    { id: 7, section: "ilustracion", title: "Origen",  images: [
       "/imagenes/trabajos/origen_1.png",
